162e299fdd4571924ffdb29f2bf4ff000ca38d6b
Heng <hfang@kth.se>
2022-03-03 22:06:37 +0000

    evaluate on the v2 experiment of 0.1 [0.8 0.1 0.1]

--- visualization_loss_miou.py
+++ visualization_loss_miou.py
@@ -2,7 +2,11 @@ import re
 import matplotlib.pyplot as plt
 import os.path as osp
 
-fullpath = osp.abspath('./slurm-1727776.out')
+# flag: 0 -> only one slurm-xx.out file
+# flag: 1 -> two slurm-xx.out files (train for twice / extra epoch needed)
+flag = 1
+
+fullpath = osp.abspath('./slurm-2133611.out')
 filedir, filename = osp.split(fullpath)
 
 print(fullpath)
@@ -14,7 +18,7 @@ with open(fullpath, 'rb') as f:
     while True:
         line = f.readline()
         line = line.decode('utf-8')
-        if line == 'slurmstepd: error: *** JOB 1727776 ON node016 CANCELLED AT 2022-02-24T08:20:49 DUE TO TIME LIMIT ***\n':
+        if line == 'Done\n':
             break
         if not line.startswith('Epoch: ['):
             continue
@@ -70,6 +74,71 @@ with open(fullpath, 'rb') as f:
 
         tcr_11_loss.append(current_tcr_11_loss)
 
+if flag == 1:
+    extrapath = osp.abspath('./slurm-2779461.out')
+
+    print(extrapath)
+
+    with open(extrapath, 'rb') as f:
+        while True:
+            line = f.readline()
+            line = line.decode('utf-8')
+            if line == 'Done\n':
+                break
+            if not line.startswith('Epoch: ['):
+                continue
+
+            # print(line)
+
+            # Example: Epoch: [70/140] Iter:[0/801], Time: 8.34, lr: [0.0005358867312681466], 
+            # Loss: 0.027249, CE_Loss: 0.262346, TCR_01_Loss: -0.000181, TCR_00_Loss: 0.011601, TCR_11_Loss: 0.001113
+            _, start_epoch = re.search('Epoch: \[', line, flags=0).span()
+            end_epoch, _ = re.search('/140]', line, flags=0).span()
+            current_epoch = float(line[start_epoch:end_epoch])
+            # print(current_epoch)
+
+            _, start_iter = re.search('Iter:\[', line, flags=0).span()
+            end_iter, _ = re.search('/801]', line, flags=0).span()
+            current_iter = float(line[start_iter:end_iter])
+            # print(current_iter)
+
+            iters.append(current_epoch * 801 + current_iter)
+
+            _, start_loss = re.search('Loss: ', line, flags=0).span()
+            end_loss, _ = re.search(', CE_Loss: ', line, flags=0).span()
+            current_loss = float(line[start_loss:end_loss])
+            # print(current_loss)
+
+            loss.append(current_loss)
+
+            _, start_ce_loss = re.search(', CE_Loss: ', line, flags=0).span()
+            end_ce_loss, _ = re.search(', TCR_01_Loss: ', line, flags=0).span()
+            current_ce_loss = float(line[start_ce_loss:end_ce_loss])
+            # print(current_ce_loss)
+
+            ce_loss.append(current_ce_loss)
+
+            _, start_tcr_01_loss = re.search(', TCR_01_Loss: ', line, flags=0).span()
+            end_tcr_01_loss, _ = re.search(', TCR_00_Loss: ', line, flags=0).span()
+            current_tcr_01_loss = float(line[start_tcr_01_loss:end_tcr_01_loss])
+            # print(current_tcr_01_loss)
+
+            tcr_01_loss.append(current_tcr_01_loss)
+
+            _, start_tcr_00_loss = re.search(', TCR_00_Loss: ', line, flags=0).span()
+            end_tcr_00_loss, _ = re.search(', TCR_11_Loss: ', line, flags=0).span()
+            current_tcr_00_loss = float(line[start_tcr_00_loss:end_tcr_00_loss])
+            # print(current_tcr_00_loss)
+
+            tcr_00_loss.append(current_tcr_00_loss)
+
+            _, start_tcr_11_loss = re.search(', TCR_11_Loss: ', line, flags=0).span()
+            end_tcr_11_loss, _ = re.search('\n', line, flags=0).span()
+            current_tcr_11_loss = float(line[start_tcr_11_loss:end_tcr_11_loss])
+            # print(current_tcr_11_loss)
+
+            tcr_11_loss.append(current_tcr_11_loss)
+
 plt.figure(1)
 plt.plot(iters, loss, label="loss")
 plt.plot(iters, ce_loss, label="ce_loss")
@@ -110,7 +179,7 @@ with open(fullpath, 'rb') as f:
     while True:
         line = f.readline()
         line = line.decode('utf-8')
-        if line == 'slurmstepd: error: *** JOB 1727776 ON node016 CANCELLED AT 2022-02-24T08:20:49 DUE TO TIME LIMIT ***\n':
+        if line == 'Done\n':
             break
         if not line.startswith('Loss: '):
             continue
@@ -134,6 +203,36 @@ with open(fullpath, 'rb') as f:
         val_miou.append(current_miou)
 
         epoch += 1
+
+if flag == 1:
+    with open(extrapath, 'rb') as f:
+        while True:
+            line = f.readline()
+            line = line.decode('utf-8')
+            if line == 'Done\n':
+                break
+            if not line.startswith('Loss: '):
+                continue
+            # print(line)
+
+            # Example: Loss: 0.250, MeanIU:  0.4352, Best_mIoU:  0.4352
+            epochs.append(epoch)
+
+            _, start_val_loss = re.search('Loss: ', line, flags=0).span()
+            end_val_loss, _ = re.search(', MeanIU:  ', line, flags=0).span()
+            current_val_loss = float(line[start_val_loss:end_val_loss])
+            # print(current_val_loss)
+
+            val_loss.append(current_val_loss)
+
+            _, start_miou = re.search(', MeanIU:  ', line, flags=0).span()
+            end_miou, _ = re.search(', Best_mIoU:  ', line, flags=0).span()
+            current_miou = float(line[start_miou:end_miou])
+            # print(current_miou)
+
+            val_miou.append(current_miou)
+
+            epoch += 1
 
 plt.figure(5)
 plt.plot(epochs, val_loss)
